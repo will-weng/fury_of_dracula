a86b4e60eb3a25cab8ccf7a992c9a93f3e8ac543
wilzlerr <william.weng@hotmail.com>
2016-09-21 17:10:54 +1000

    still working on newGameView, added a lot of static functions to help

--- GameView.c
+++ GameView.c
@@ -5,13 +5,20 @@
 #include "Globals.h"
 #include "Game.h"
 #include "GameView.h"
+#include "Places.h"
 // #include "Map.h" ... if you decide to use the Map ADT
      
-static int score(char *pastPlays);
-static int hospital(char *pastPlays);
-static int matured(char *pastPlays);
+static void readPlay(GameView currentView);
+static void draculaPlays(char *play, GameView currentView);
+static void hunterPlays(char *play, GameView currentView);
+
+static void score(GameView currentView);
+// static void health(GameView currentView);
 
 static int rounds;
+static int hospital;
+static int matured;
+static int draculaHealth;
 
 #define STRING_OF_ROUND 40
 #define STRING_OF_MOVE 8
@@ -33,11 +40,20 @@ GameView newGameView(char *pastPlays, PlayerMessage messages[])
 {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
     GameView gameView = malloc(sizeof(struct gameView));
+    gameView->pastPlays = pastPlays;
     gameView->roundNum = (sizeof(pastPlays)/(STRING_OF_ROUND*sizeof(char)));
     rounds = gameView->roundNum;
     gameView->player = (rounds % 5);
-    //gameView->health = 
-    gameView->score = score(gameView->pastPlays);
+
+    int counter;
+    for(counter = 0; counter < 4; counter++) {
+        gameView->health[counter] = GAME_START_HUNTER_LIFE_POINTS;
+    }
+
+    readPlay(gameView);
+
+    score(gameView);
+
     return gameView;
 }
      
@@ -112,45 +128,102 @@ LocationID *connectedLocations(GameView currentView, int *numLocations,
     return NULL;
 }
 
+// static function that reads the pastplay string
+static void readPlay(GameView currentView) {
+    
+    draculaHealth = GAME_START_BLOOD_POINTS;
+    hospital = 0;
+    matured = 0;
+    char play[STRING_OF_MOVE] = {0};
+    int counter, string;
+
+    while(currentView->pastPlays[counter] != 0) {
+        for(string = 0; string < STRING_OF_MOVE; counter++, string++) {
+            play[string] = currentView->pastPlays[counter];
+        }
+        if(play[0] == 'D') {
+            draculaPlays(play, currentView);
+        } else {
+            hunterPlays(play, currentView);
+        }
+    }
+}
+
+// static function to read the dracula's move
+static void draculaPlays(char *play, GameView currentView) {
+
+    char abbrev[3] = {0};
+    abbrev[0] = play[1];
+    abbrev[1] = play[2];
+    abbrev[2] = 0;
+
+    if(play[6] == 'V') {
+        matured++;
+    }
+
+    currentView->currLocation[PLAYER_DRACULA] = abbrevToID(abbrev);
+
+    if(idToType(currentView->currLocation[PLAYER_DRACULA]) == SEA) {
+        draculaHealth = draculaHealth - LIFE_LOSS_SEA;
+    } else if(currentView->currLocation[PLAYER_DRACULA] == CASTLE_DRACULA ||
+            (abbrev[0] == 'T' && abbrev[1] == 'P')) {
+        draculaHealth = draculaHealth + LIFE_GAIN_CASTLE_DRACULA;
+    }
+}
+
+static void hunterPlays(char *play, GameView currentView) {
+
+    char abbrev[3] = {0};
+    abbrev[0] = play[1];
+    abbrev[1] = play[2];
+    abbrev[2] = 0;
+
+    int player;
+
+    if(play[0] == 'G') {
+        player = PLAYER_LORD_GODALMING;
+    } else if(play[0] == 'S') {
+        player = PLAYER_DR_SEWARD;
+    } else if(play[0] == 'H') {
+        player = PLAYER_VAN_HELSING;
+    } else if(play[0] == 'M') {
+        player = PLAYER_MINA_HARKER;
+    }
+
+}
+
+/*
+// static function to calculate health of all players
+static void health(GameView currentView) {
+    
+    hospital = 0;
+    int string, counter = 0;
+    char readPlay[STRING_OF_MOVE] = {0};
+    char lastPlace[]
+
+    while(pastPlays[counter] != NULL) {
+        for(string = 0; string < STRING_OF_MOVE; counter++, string++) {
+            readPlay[counter] = pastPlays[counter];
+        }
+
+        if(readPlay[])
+    }
+}
+*/
+
 // static function to calculate current score of the game
-static int score(char *pastPlays) {
+static void score(GameView currentView) {
     // initialzing total score to 366
     int totalScore = GAME_START_SCORE;
 
     // decreasing score by hospital visits
-    totalScore = totalScore - (SCORE_LOSS_HUNTER_HOSPITAL * hospital(pastPlays));
+    totalScore = totalScore - (SCORE_LOSS_HUNTER_HOSPITAL * hospital);
 
     // decreasing score by vampires matured
-    totalScore = totalScore - (SCORE_LOSS_VAMPIRE_MATURES * matured(pastPlays));
+    totalScore = totalScore - (SCORE_LOSS_VAMPIRE_MATURES * matured);
 
     // decrease score by the rounds played
     totalScore = totalScore - rounds;
     
-    return totalScore;
-}
-
-// static function to calculate hospital visits used to calculate score
-static int hospital(char *pastPlays) {
-    int visits = 0;e
-
-//    char moveRead[STRING_OF_MOVES] = {};
-
-    return visits;
-}
-
-// static function to calculate vampires matured used to calculate score
-static int matured(char *pastPlays) {
-
-    int vampires = 0;
-    int counter;
-    int firstMature = 6 * STRING_OF_ROUND + 37;
-
-    // reads for if a vampire matures
-    for(counter = 0; counter * PERIOD_OF_VAMPIRE_SPAWN + 6 < rounds; counter++) {
-        if(pastPlays[firstMature + STRING_OF_ROUND * PERIOD_OF_VAMPIRE_SPAWN * counter] == 'V') {
-            vampires++;
-        }
-    }
-
-    return vampires;
+    currentView->score = totalScore;
 }
