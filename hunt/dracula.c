22377d93dd2e4669aa1a9022210356d5c8b52946
wilzlerr <william.weng@hotmail.com>
2016-10-07 23:59:26 +1100

    tried working on draculamove but it's not working

--- hunt/dracula.c
+++ hunt/dracula.c
@@ -5,10 +5,21 @@
 #include <stdio.h>
 #include "Game.h"
 #include "DracView.h"
+#include "Places.h"
 
 void decideDraculaMove(DracView gameState)
 {
 	// TODO ...
 	// Replace the line below by something better
-	registerBestPlay("CD","Mwuhahahaha");
+    int location = 10;
+    int places[10];
+    LocationID *array = whereCanIgo(gameState, &location,TRUE,FALSE);  
+    int i;
+    for(i = 10; i > 1; i--)
+    {
+        places[i] = array[i];
+    }
+    
+    printf("%s", idToName(places[3]));
+	registerBestPlay(idToName(places[3]),"Mwuhahahaha");
 }
