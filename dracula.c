14d5fcb83ca5a0050e17d628782690f7c5fc3d93
wilzlerr <william.weng@hotmail.com>
2016-10-09 02:19:29 +1100

    changed some stuff

--- dracula.c
+++ dracula.c
@@ -17,7 +17,7 @@ void decideDraculaMove(DracView gameState)
     char *play = "KL";
     char message[MESSAGE_SIZE] = "YOU CAN'T CATCH ME";
     int numLocations, hunters[NUM_OF_HUNTERS], counter;
-    LocationID *places;
+    LocationID *path;
 
     // stores where hunteres are for easy access
     for(counter = 0; counter < NUM_OF_HUNTERS; counter++)
@@ -28,8 +28,8 @@ void decideDraculaMove(DracView gameState)
 
     // move randomly after initial rounds
     if(giveMeTheRound(gameState) > 2) {
-        places = whereCanIgo(gameState, &numLocations, TRUE, FALSE);
-        play = idToAbbrev(places[numLocations/2]);
+        path = whereCanIgo(gameState, &numLocations, TRUE, FALSE);
+        play = idToAbbrev(path[numLocations/2]);
     }
 
     // if reaches some far places hopefully would tp to castle
@@ -39,7 +39,7 @@ void decideDraculaMove(DracView gameState)
     // move to different place if chosen one has a hunter on it
     for(counter = 0; counter < NUM_OF_HUNTERS; counter++) {
         if(strcmp(play, idToAbbrev(hunters[counter])) == 0)
-            play = idToAbbrev(places[rand() % numLocations]);
+            play = idToAbbrev(path[rand() % numLocations]);
     }
 
     registerBestPlay(play, message);
