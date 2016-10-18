96debe948ddd3899235c571b75e55359442b9068
wilzlerr <william.weng@hotmail.com>
2016-10-18 15:46:58 +1100

    worked on hunters

--- hunter.c
+++ hunter.c
@@ -7,39 +7,56 @@
 #include "HunterView.h"
 #include "Places.h"
 
+static struct
+{
+    LocationID currLocation[NUM_PLAYERS];
+    LocationID *places;
+    int round;
+    int me;
+} gameInfo;
 
 void decideHunterMove(HunterView gameState)
 {
     // initializes variables
     char *play = "CD";
     char message[MESSAGE_SIZE] = "";
-    int numLocations, me;
-    LocationID *places = whereCanIgo(gameState, &numLocations, TRUE, TRUE, TRUE);
+    int numLocations, counter;
 
-    // find out which hunters turn it is
-    me = whoAmI(gameState);
-    
+    // fills out the game info struct
+    gameInfo.me = whoAmI(gameState);
+    gameInfo.round = giveMeTheRound(gameState);
+    gameInfo.places = whereCanIgo(gameState, &numLocations, TRUE, TRUE, TRUE);
+    for(counter = 0; counter < NUM_PLAYERS; counter++) {
+        gameInfo.currLocation[counter] = whereIs(gameState, counter);
+    }
+
+
     // individual decisions of hunters
-    if(me == PLAYER_LORD_GODALMING)
+    if(gameInfo.me == PLAYER_LORD_GODALMING)
     {
     }
-    else if(me == PLAYER_DR_SEWARD)
+    else if(gameInfo.me == PLAYER_DR_SEWARD)
     {
         play = "FR";
     }
-    else if(me == PLAYER_VAN_HELSING)
+    else if(gameInfo.me == PLAYER_VAN_HELSING)
     {
         play = "MN";
     }
-    else if(me == PLAYER_MINA_HARKER)
+    else if(gameInfo.me == PLAYER_MINA_HARKER)
     {
         play = "SR";
     }
 
     // chooses a random place to go
-    if(giveMeTheRound(gameState) > 0) play = idToAbbrev(places[numLocations/2]);
+    if(gameInfo.round == 7) {
+        play = idToAbbrev(gameInfo.currLocation[gameInfo.me]);
+    } else if(gameInfo.round > 0) {
+        play = idToAbbrev(gameInfo.places[numLocations/2]);
+    }
+
     // if hunter is lower than 5 health, it would rest
-    if(howHealthyIs(gameState, me) < 5) play = idToAbbrev(whereIs(gameState, me));
+    if(howHealthyIs(gameState, gameInfo.me) < 5) play = idToAbbrev(whereIs(gameState, gameInfo.me));
 
     registerBestPlay(play, message);
 }
